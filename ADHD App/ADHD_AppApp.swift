fd6fd0c92384c08511f8323dcc22caace9938cbc
Ariv <arivsahoo@gmail.com>
2024-02-15 23:35:27 -0800

    Initial Commit

---
+++ ADHD App/ADHD_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADHD_AppApp.swift
+//  ADHD App
+//
+//  Created by Ariv Sahoo on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ADHD_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
